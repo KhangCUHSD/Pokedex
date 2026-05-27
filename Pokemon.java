f86382e1870a6f79e20b6390d14c36df603d3910
KhangCUHSD <119456676+KhangCUHSD@users.noreply.github.com>
2026-05-27 10:35:26 -0700

    Update Pokemon.java
    Updated

--- Pokemon.java
+++ Pokemon.java
@@ -1,29 +1,29 @@
 public class Pokemon {
     private int id, total, hp, attack, defense, specialAttack, specialDefense, speed;
     private String name, type1, type2;
-    
-    public Pokemon (int id, String name, String type1, String type2, int total, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed) {
+
+    public Pokemon(int id, String name, String type1, String type2, int total, int hp, int attack, int defense, int specialAttack, int specialDefense, int speed) {
         this.id = id;
         this.name = name;
-        this.type1 = t1;
-        this.type2 = t2;
-        this.total = bst;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.total = total;
         this.hp = hp;
-        this.attack = atk;
-        this.defense = def;
-        this.specialAttack = spAtk;
-        this.specialDefense = spDef;
-        this.speed = spe;
+        this.attack = attack;
+        this.defense = defense;
+        this.specialAttack = specialAttack;
+        this.specialDefense = specialDefense;
+        this.speed = speed;
     }
+
     public int getID() { return id; }
     public String getName() { return name; }
-    public String getType1() { return t1; }
-    public String getType2() { return t2; }
+    public String getType1() { return type1; }
+    public String getType2() { return type2; }
     public int getHealth() { return hp; }
-    public int getAttack() { return atk; }
-    public int getDefense() { return def; }
-    public int getSp.Atk() { return spAtk; }
-    public int getSp.Def() { return spDef; }
-    public int getSpeed() { return spe; }
-    }
+    public int getAttack() { return attack; }
+    public int getDefense() { return defense; }
+    public int getSpAtk() { return specialAttack; }
+    public int getSpDef() { return specialDefense; }
+    public int getSpeed() { return speed; }
 }
